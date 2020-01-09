d47b8ee0f939d53a5d32aa39a8679248934993fc
Giorgi Kavrelishvili <gkavrelishvili@protonmail.com>
2020-01-09 12:54:27 +0400

    Fixed the invalid memory access bug and formated the code

--- src/grip/db/base.cr
+++ src/grip/db/base.cr
@@ -1,29 +1,29 @@
 require "leveldb"
 
 module Grip::DB
-    class Base
-        getter path : Symbol
+  class Base
+    getter path : Symbol
 
-        def initialize(@path : Symbol); end
+    def initialize(@path : Symbol); end
 
-        def database : LevelDB::DB
-            @_database ||= LevelDB::DB.new(@path.to_s)
-        end
-
-        def open(&block)
-            yield database
-        end
-
-        def get(key : String)
-            open do |db|
-                db.get(key)
-            end
-        end
-
-        def set(key : String, value : String)
-            open do |db|
-                db.put(key, value)
-            end
-        end
+    def database : LevelDB::DB
+      @_database ||= LevelDB::DB.new(@path.to_s)
     end
+
+    def open(&block)
+      yield database
+    end
+
+    def get(key : String)
+      open do |db|
+        db.get(key)
+      end
+    end
+
+    def set(key : String, value : String)
+      open do |db|
+        db.put(key, value)
+      end
+    end
+  end
 end
