f976938124241611d1e91bbc174e230844225641
Giorgi Kavrelishvili <gkavrelishvili@protonmail.com>
2020-01-09 14:44:03 +0400

    Invalid memory access exceptions

--- src/grip/db/base.cr
+++ src/grip/db/base.cr
@@ -4,10 +4,12 @@ module Grip::DB
   class Base
     getter path : String
 
-    def initialize(@path : String); end
+    def initialize(@path : String)
+      @db = LevelDB::DB.new(@path)
+    end
 
     def database : LevelDB::DB
-      @_database ||= LevelDB::DB.new(@path)
+      @_database ||= @db
     end
 
     def open(&block)
@@ -16,12 +18,24 @@ module Grip::DB
 
     def get(key : String)
       open do |db|
+        while !db.opened?
+          db.open
+          if db.opened?
+            break
+          end
+        end
         db.get(key)
       end
     end
 
     def set(key : String, value : String)
       open do |db|
+        while !db.opened?
+          db.open
+          if db.opened?
+            break
+          end
+        end
         db.put(key, value)
       end
     end
--- src/grip/db/macros.cr
+++ src/grip/db/macros.cr
@@ -1,11 +1,14 @@
-def storage(path, &block)
-    yield Grip::DB::Connections[path]
-end
-
-macro store(key, value)
-    db.set({{key}}, {{value}}.to_json)
-end
-
-macro retrieve(key)
-    db.get({{key}}).to_s
+module Grip::DB
+    macro parent(path)
+        Grip::DB::Connections << Grip::DB::Base.new({{path}})
+    end
+    def storage(path, &block)
+        yield Grip::DB::Connections[path]
+    end
+    macro store(key, value)
+        db.set({{key}}, {{value}}.to_json)
+    end
+    macro retrieve(key)
+        db.get({{key}}).to_s
+    end
 end
