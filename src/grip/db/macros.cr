9c8294a9ef95a4411286c428a6a6df944d95b8ff
Giorgi Kavrelishvili <gkavrelishvili@protonmail.com>
2020-01-09 14:16:44 +0400

    Updated the macros and methods

--- src/grip/db/macros.cr
+++ src/grip/db/macros.cr
@@ -1,11 +1,11 @@
-macro storage(path)
-    @db = Grip::DB::Connections[{{path}}]
+def storage(path, &block)
+    yield Grip::DB::Connections[path]
 end
 
 macro store(key, value)
-    @db.set({{key}}, {{value}}.to_json)
+    db.set({{key}}, {{value}}.to_json)
 end
 
 macro retrieve(key)
-    @db.get({{key}}).to_s
+    db.get({{key}}).to_s
 end
