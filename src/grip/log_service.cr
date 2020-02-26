c1cef1584c5557a1476ed0ab2658b2069aff18fb
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2020-02-26 13:52:04 +0400

    Added the error codes in case of something goes wrong

--- src/grip/log_service.cr
+++ src/grip/log_service.cr
@@ -2,24 +2,49 @@ module Grip
     class LogService
         include SimpleRpc::Proto
 
-        def info(msg : String) : Void
-            Grip.config.logger.write "[\u001b[34minfo\u001b[0m] #{msg}\n"
+        def info(msg : String) : Int32
+            begin
+               Grip.config.logger.write "[\u001b[34minfo\u001b[0m] #{msg}\n" 
+               0
+            rescue _exception
+               1
+            end
         end
 
-        def debug(msg : String) : Void
-            Grip.config.logger.write "[\u001b[35mdebug\u001b[0m] #{msg}\n"
+        def debug(msg : String) : Int32
+            begin
+               Grip.config.logger.write "[\u001b[35mdebug\u001b[0m] #{msg}\n" 
+               0
+            rescue _exception
+               1
+            end
         end
 
-        def message(msg : String) : Void
-            Grip.config.logger.write "[\u001b[36mmessage\u001b[0m] #{msg}\n"
+        def message(msg : String) : Int32
+            begin
+               Grip.config.logger.write "[\u001b[36mmessage\u001b[0m] #{msg}\n" 
+               0
+            rescue _exception
+               1
+            end
         end
 
-        def warning(msg : String) : Void
-            Grip.config.logger.write "[\u001b[33mwarning\u001b[0m] #{msg}\n"
+        def warning(msg : String) : Int32
+            begin
+               Grip.config.logger.write "[\u001b[33mwarning\u001b[0m] #{msg}\n" 
+               0
+            rescue _exception
+               1
+            end
         end
 
-        def error(msg : String) : Void
-            Grip.config.logger.write "[\u001b[31merror\u001b[0m] #{msg}\n"
+        def error(msg : String) : Int32
+            begin
+               Grip.config.logger.write "[\u001b[31merror\u001b[0m] #{msg}\n" 
+               0
+            rescue _exception
+               1
+            end
         end
     end
 end
