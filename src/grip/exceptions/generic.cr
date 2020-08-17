75ecd8a14854b134c83572b34df77142245071e5
Giorgi Kavrelishvili <grrkek@Giorgis-MacBook-Pro.local>
2020-08-17 16:38:52 +0400

    Updated the exception handling

--- src/grip/exceptions/generic.cr
+++
@@ -1,9 +0,0 @@
-module Grip
-  module Exceptions
-    class Generic < Exception
-      def initialize(context : HTTP::Server::Context)
-        super "Sorry, something went wrong, please try again later."
-      end
-    end
-  end
-end
