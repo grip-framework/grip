eb7e9e1cde415379bab51f7d61a0489d16ee2eae
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2020-09-23 09:08:30 +0400

    Default response code changed to 500

--- src/grip/handlers/exception.cr
+++ src/grip/handlers/exception.cr
@@ -8,7 +8,7 @@ module Grip
       def call(context : HTTP::Server::Context)
         call_next(context)
       rescue ex
-        context.response.status_code = 404 if !context.response.status_code.in?([400, 401, 403, 404, 405, 500])
+        context.response.status_code = 500 if !context.response.status_code.in?([400, 401, 403, 404, 405, 500])
 
         if ex.is_a?(Grip::Exceptions::Base)
           return call_exception_with_status_code(context, ex, ex.status_code)
