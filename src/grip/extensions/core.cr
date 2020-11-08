fcb36b36cff46725ebd75396ef0a6e17be3de272
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2020-11-08 11:33:26 +0400

    Added number, string validation

--- src/grip/extensions/core.cr
+++ src/grip/extensions/core.cr
@@ -11,3 +11,11 @@ end
 struct NamedTuple
   include Grip::Extensions::NamedTuple
 end
+
+class String
+  include Grip::Extensions::String
+end
+
+abstract struct Number
+  include Grip::Extensions::Number
+end
